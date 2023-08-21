062422b4cac4bce9fa8d21815cdb17cdf93d830c
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-08-21 11:57:18 +0530

    data fetching for SSG done

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,38 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async() => {
+    return {
+        fallback : false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                }
+            }
+        ]
+    }
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbi_4-mbmDG3SOE8P5OleU7PiJWti7zypc2g&usqp=CAU",
+        title: "Meetup",
+        address: "some address",
+        description: "some description",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
