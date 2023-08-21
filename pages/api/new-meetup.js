23e1a1781893700c7bbde279870256382ee7cc99
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-08-21 13:14:22 +0530

    mongodb connection logic added

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://BhanuPratap:EvlE3EwVBxJrrW1u@cluster0.utzcfrg.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "meetup inserted" });
+  }
+};
+
+export default handler;
