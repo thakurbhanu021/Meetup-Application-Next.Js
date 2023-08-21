9e94b0f331c1cacadc19c2a8ca536377848c1551
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-08-21 14:01:06 +0530

    fetching meetup list from mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -7,18 +7,19 @@ const handler = async (req, res) => {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://BhanuPratap:EvlE3EwVBxJrrW1u@cluster0.utzcfrg.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://BhanuPratap:8979465461India@cluster0.utzcfrg.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
 
     const meetupsCollection = db.collection("meetups");
 
     const result = await meetupsCollection.insertOne(data);
+
     console.log(result);
 
     client.close();
 
-    res.status(201).json({ message: "meetup inserted" });
+    res.status(201).json({ message: "Meetup inserted!"});
   }
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
+import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "First Meetup",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbi_4-mbmDG3SOE8P5OleU7PiJWti7zypc2g&usqp=CAU",
-    address: "Some address 10, 12345 Some City",
-    description: "This is First meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbi_4-mbmDG3SOE8P5OleU7PiJWti7zypc2g&usqp=CAU",
-    address: "Some address 10, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "First Meetup",
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbi_4-mbmDG3SOE8P5OleU7PiJWti7zypc2g&usqp=CAU",
+//     address: "Some address 10, 12345 Some City",
+//     description: "This is First meetup!",
+//   },
+//   {
+//     id: "m2",
+//     title: "A Second Meetup",
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbi_4-mbmDG3SOE8P5OleU7PiJWti7zypc2g&usqp=CAU",
+//     address: "Some address 10, 12345 Some City",
+//     description: "This is a second meetup!",
+//   },
+// ];
 
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
@@ -35,9 +37,25 @@ const HomePage = (props) => {
 // }
 
 export async function getStaticProps(){
+
+    const client = await MongoClient.connect('mongodb+srv://BhanuPratap:8979465461India@cluster0.utzcfrg.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map((meetup)=> ({
+                    title: meetup.title,
+                    image: meetup.image,
+                    address: meetup.address,
+                    id: meetup._id.toString(),
+            }))
         },
         revalidate: 1
     }
